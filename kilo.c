bfe8e39815aa173bb7f9967195d169749e56e0e7
Niko Steinhoff <niko.steinhoff@pm.me>
2023-02-24 21:22:39 +0100

    Add raw terminal input and output

--- kilo.c
+++ kilo.c
@@ -1,33 +1,65 @@
+/*** includes ***/
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
-#include <stdlib.h>
+
+/*** defines ***/
+
+#define NORETURN __attribute__((noreturn)) void
+
+/*** declarations ***/
+
+static void die(const char *s);
+static void enable_raw_mode(void);
+static void reset_term(void);
+
+/*** data ***/
 
 static struct termios term_orig;
 
-static void enable_raw_mode(void);
-static void reset_term(void);
+/*** main ***/
 
 int main(void) {
 	enable_raw_mode();
 
-	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
-		;
+	for (char c = 0; c != 'q';) {
+		c = 0;
+		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+			die("read");
+		if (iscntrl(c)) printf("%d\r\n", c);
+		else printf("%d ('%c')\r\n", c, c);
+	}
 
 	return 0;
 }
 
-static void reset_term(void) {
-	tcsetattr(STDIN_FILENO, TCSANOW, &term_orig);
+/*** terminal ***/
+
+static NORETURN die(const char *s) {
+	perror(s);
+	exit(1);
 }
 
 static void enable_raw_mode(void) {
 	// Save terminal settings and setup cleanup on exit.
-	tcgetattr(STDIN_FILENO, &term_orig);
+	if (tcgetattr(STDIN_FILENO, &term_orig) == -1) die("tcgetattr");
 	atexit(reset_term);
 
 	// Set terminal to raw mode.
 	struct termios term_raw = term_orig;
-	term_raw.c_lflag &= ~(unsigned)(ECHO);
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &term_raw);
+	term_raw.c_iflag &= ~(unsigned)(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	term_raw.c_oflag &= ~(unsigned)(OPOST);
+	term_raw.c_cflag |= (unsigned)(CS8);
+	term_raw.c_lflag &= ~(unsigned)(ECHO | ICANON | ISIG | IEXTEN);
+	term_raw.c_cc[VMIN] = 0;
+	term_raw.c_cc[VTIME] = 1;
+	if (tcsetattr(STDIN_FILENO, TCSANOW, &term_raw) == -1) die("tcsetattr");
+}
+
+static void reset_term(void) {
+	if (tcsetattr(STDIN_FILENO, TCSANOW, &term_orig) == -1)
+		die("tcsetattr");
 }
